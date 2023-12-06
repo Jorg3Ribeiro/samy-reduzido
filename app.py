68208e31afcf4172ee71c952d431d65c30aa60d6
Jorg3Ribeiro <31208071+Jorg3Ribeiro@users.noreply.github.com>
2023-12-06 15:35:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,42 +15,49 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Data(BaseModel):
+    method: str, 
+    language: str, 
+    sentence_count: int, 
+    input_type: str, 
+    input_: str
+
 @app.post("/summarize")
-def summarize(method, language, sentence_count, input_type, input_):
-  if method== 'LSA':
+def summarize(data: Data):
+  if data.method== 'LSA':
     from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-  if method=='text-rank':
+  if data.method=='text-rank':
     from sumy.summarizers.text_rank import TextRankSummarizer as Summarizer
-  if method=='lex-rank':
+  if data.method=='lex-rank':
     from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
-  if method=='edmundson':
+  if data.method=='edmundson':
     from sumy.summarizers.edmundson import EdmundsonSummarizer as Summarizer
-  if method=='luhn':
+  if data.method=='luhn':
     from sumy.summarizers.luhn import LuhnSummarizer as Summarizer
-  if method=='kl-sum':
+  if data.method=='kl-sum':
     from sumy.summarizers.kl import KLSummarizer as Summarizer
-  if method=='random':
+  if data.method=='random':
     from sumy.summarizers.random import RandomSummarizer as Summarizer
-  if method=='reduction':
+  if data.method=='reduction':
     from sumy.summarizers.reduction import ReductionSummarizer as Summarizer
 
-  if input_type=="URL":
-    parser = HtmlParser.from_url(input_, Tokenizer(language))
-  if input_type=="text":
-    parser = PlaintextParser.from_string(input_, Tokenizer(language))
+  if data.input_type=="URL":
+    parser = HtmlParser.from_url(data.input_, Tokenizer(data.language))
+  if data.input_type=="text":
+    parser = PlaintextParser.from_string(data.input_, Tokenizer(data.language))
     
-  stemmer = Stemmer(language)
+  stemmer = Stemmer(data.language)
   summarizer = Summarizer(stemmer)
-  stop_words = get_stop_words(language)
+  stop_words = get_stop_words(data.language)
 
-  if method=='edmundson':
+  if data.method=='edmundson':
     summarizer.null_words = stop_words
     summarizer.bonus_words = parser.significant_words
     summarizer.stigma_words = parser.stigma_words
   else:
     summarizer.stop_words = stop_words
 
-  summary_sentences = summarizer(parser.document, sentence_count)
+  summary_sentences = summarizer(parser.document, data.sentence_count)
   summary = ' '.join([str(sentence) for sentence in summary_sentences])
   
   return summary
